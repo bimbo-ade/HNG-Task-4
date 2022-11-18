ed734787674988e6231a2315119b27c4a35c6e5f
bimbo-ade <bimboadeyemi65@gmail.com>
2022-11-18 18:16:27 +0100

    feat: questions component

---
+++ src/components/Questions.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Questions = () => {
+  return (
+    <div className="questions-container">
+      <h4>FAQ</h4>
+      <h1>Frequently Asked Questions</h1>
+      <p>
+        Get quick answers to questions about HNG internship, teams, projects and
+        others
+      </p>
+    </div>
+  );
+};
+
+export default Questions;
--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Questions from "../components/Questions";
 import Teams from "../components/Teams";
 
 const FAQ = () => {
   return (
     <div>
       <Navbar />
+      <Questions />
       <Teams />
       <Footer />
     </div>
