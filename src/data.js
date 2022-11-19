a7b94f0b2a0b9f33d9c44343f81bf5fdfa2fe267
bimbo-ade <bimboadeyemi65@gmail.com>
2022-11-19 07:17:22 +0100

    added question data

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const data = [
+  {
+    Question: " What is the Internship all about?",
+    Answer:
+      " It is a virtual and remote internship that enables you to learn and improve your skills by working on real-world projects. It is for people learning to code/design/manage and want to accelerate their knowledge.",
+  },
+  {
+    Question: " What benefits can I get from the internship?",
+    Answer:
+      " You will rapidly improve your technical skills. You will meet other smart people. You will build a great network that will serve you for life.",
+  },
+  {
+    Question: " How do we work in teams?",
+    Answer:
+      " You collaborate with your teammates and leads to deliver on your projects and task",
+  },
+  {
+    Question: " How long is the internship?",
+    Answer: " About 7 weeks",
+  },
+  {
+    Question: " Will mentors be available for physical mentorship?",
+    Answer:
+      " Meetups will be held where mentors around your region will be available to answer any question.",
+  },
+  {
+    Question: " How often do we build new projects?",
+    Answer:
+      " Every week new projects are assigned to interns as tasks to complete and present to be promoted to the next stage.",
+  },
+
+  {
+    Question: " What tracks are available?",
+    Answer:
+      " We offer Backend, Frontend, DevOps, Mobile, UI/UX, Sales/Marketing and Management tracks.",
+  },
+  {
+    Question: " How many stages is the internship?",
+    Answer: " The internship has 10 stages",
+  },
+];
